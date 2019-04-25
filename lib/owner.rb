b1908bd547d77f9c70564c777c2ccbf1e7fe672a
roy renno <royrenno@yahoo.com>
2019-04-25 00:36:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,6 +7,7 @@ class Owner
     @species = species
 
     @pets ={:fishes=> [], :cats=> [], :dogs=> []}
+  end
 
     def say_species
       return "I am a #{@species}"
@@ -14,3 +15,4 @@ class Owner
   # code goes here
 end
 end
+
