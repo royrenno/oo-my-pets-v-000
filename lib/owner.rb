8c24114ea37ad53e91a31a4ee9911061c4bb9d9f
roy renno <royrenno@yahoo.com>
2019-04-25 01:44:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,8 +61,8 @@ class Owner
     num_fish =@pets[:fishes].size
     num_dogs =@pets[:dogs].size
     num_cats =@pets[:cats].size
-    return"I have #{num_fish}fish, #{num_dogs} dog(s),
-    and#{num_cats}cat(s)."
+    return"I have #{num_fish} fish, #{num_dogs} dog(s),
+    and#{num_cats} cat(s)."
   end
   def self.all
     @@all
