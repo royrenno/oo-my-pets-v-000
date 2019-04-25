37e3b5b463aaef49c0bc80fac07c91fe5b9a0d57
roy renno <royrenno@yahoo.com>
2019-04-25 00:14:50 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,10 @@
 class Cat
   attr_accessor :mood
-  attr_writer :name
+  attr_reader :name
 
   def initialize(name)
-    @name = name
+    @name =name
+    @mood ="nervous"
   # code goes here
 end
 end
